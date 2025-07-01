cbcdefef63ff0c8f57567e171f44446633f115e0
Zetong999 <z.liu10@student.fdu.edu>
2025-06-30 22:02:01 -0700

    test cicd

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,25 @@
-export default function ProjectPage() {
-    return (
-        <div className="text-black">
-            this is Project page
+import ProjectCard from "@/components/project";
+
+export default function ProjectsPage() {
+  return (
+    <div className="px-6 py-10 w-full">
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-3xl font-bold mb-8">Projects Header</h1>
+
+        <div className="space-y-6">
+          <ProjectCard
+            imageSrc="/envex-logo.png"
+            title="EnvEx"
+            tag="NEW"
+            description="Dotenv sample made easy!"
+          />
+          <ProjectCard
+            imageSrc="/fingervein-logo.png"
+            title="FingerVeinApp"
+            description="Machine Learning Finger Vein Authentication System"
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
